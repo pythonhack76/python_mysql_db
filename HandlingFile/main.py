287ebbbbf831c016fee2b9d06f73d94719319009
Python The Hack <lrulvoni@gmail.com>
2022-06-10 14:21:46 +0200

    update Pandas

--- HandlingFile/main.py
+++ HandlingFile/main.py
@@ -1,4 +1,5 @@
 #gestione dei files
+import os
 
 def creaFile():
     global file
@@ -13,5 +14,18 @@ def leggiFile():
     f.close() 
 
 
-creaFile()
-leggiFile() 
+def leggiFiles():
+    select_dir = input("indica tu la directory...")
+    if select_dir != True:
+        current_directory = os.getcwd()
+        files = os.listdir(current_directory)
+        for filename in files:
+            print(filename)
+    else:
+        files = os.listdir(select_dir)
+        for filename in files:
+            print(filename)
+        
+
+
+leggiFiles() 
