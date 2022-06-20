6b8ab4a77aabd1da0f7bef64a41832a09eee48ed
Python The Hack <lrulvoni@gmail.com>
2022-06-20 11:48:04 +0200

    one check list py

--- Pandas/index.py
+++ Pandas/index.py
@@ -5,11 +5,11 @@ import pandas as pd
 ds = {
     'Mesi': ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
     'Giorni': [31, 28, 31, 30,31, 30, 31, 31, 30,31, 30,31],
-    "Stagione": ["Inverno","Inverno","Primavera","Primavera","Primavera","Primavera","Estate","Estate","Estate","Estate","Autunno","Autunno","Autunno","Inverno"],
+    "Stagione": ["Inverno","Inverno","Primavera","Primavera","Primavera","Estate","Estate","Estate","Autunno","Autunno","Autunno","Inverno"],
     "Festivita": ["Capdanno","Carnevale","Festa delle Donne","Pasqua","Festa della Madonna","Spring Break","Rivoluzione Francese","Compleanno","Compleanno","Rivoluzione Russa","Festa Esercito","Natale"]
 }
 
-df = pd.DataFrame(ds)
+df = pd.DataFrame(ds, index=["Riga1", "Riga2", "Riga3","Riga4","Riga5","Riga6","Riga7","Riga8","Riga9","Riga10","Riga11","Riga12"])
 
-print(df)
+print(df.loc["Riga2"])
 
